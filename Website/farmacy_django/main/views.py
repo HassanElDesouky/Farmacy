e045faebf6259b215af4fd69314eb984adf3b2ee
Hassan El Desouky <hassaneldesouky@icloud.com>
2019-05-05 15:23:07 +0200

    Add registeration and loggin in

--- Website/farmacy_django/main/views.py
+++ Website/farmacy_django/main/views.py
@@ -1,27 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Medicine
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def homepage(request):
-
-    medicines = Medicine.objects.all()
-    search_term = ''
-
     if 'search' in request.GET:
+        medicines = Medicine.objects.all()
         search_term = request.GET['search']
         medicines = medicines.filter(medicine_name__icontains=search_term)
-        print(medicines)
+        context = {"medicines": medicines, 'search_term': search_term}
+        return render(request,
+                      'main/home.html',
+                      context)
+    else:
+        return render(request,
+                      'main/home.html')
 
-    context = {"medicines": medicines, 'search_term': search_term}
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'New account created: {username}')
+            login(request, user)
+            messages.info(request, f'You are now logged in as: {username}')
+            return redirect('main:homepage')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f'{msg}: {form.error_messages[msg]}')
+    form = UserCreationForm
     return render(request,
-                  'main/home.html',
-                  context)
+                  'main/register.html',
+                  context={'form': form})
 
 
-def details(request, medicine_id):
-    return HttpResponse('You are looking for: {}'.format(medicine_id))
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect("main:homepage")
 
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as: {username}')
+                return redirect('main:homepage')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    form = AuthenticationForm()
+    return render(request,
+                  "main/login.html",
+                  {"form": form})
